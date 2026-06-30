4958f91f614ea1c57d334117ca9295c18dd9382e
Hasan <hassandossa5@gmail.com>
2026-06-30 20:01:07 +0300

    feat: add fonts & font icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,18 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+
+        {/* Your Fonts */}
+        <link href="https://fonts.googleapis.com/css2?family=Lora:wght@400;500;700&family=Dm+Sans:wght@400;500;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@100..900&family=Outfit:wght@100..900&display=swap" rel="stylesheet" />
+
+        {/* Material Icons */}
+        <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet" />
+      </head>
+      <body className="bg-background text-on-background">{children}</body>
     </html>
   );
 }
